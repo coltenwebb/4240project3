9ff164a0fb8f284b720a0a315bf4629b53c5bb94
Ethan Kiang <chocopuff298@gmail.com>
2022-04-10 05:10:32 -0400

    added remove direct left recursion

--- app/LL1.hs
+++ app/LL1.hs
@@ -1,16 +1,31 @@
 module LL1 where
 
 import qualified Data.Map as M 
+import Production
 
 type ReductionRule = Production -> [Production]
-type symbol
 
-removeIndirectLeftRecursion :: Grammar -> Grammar 
+--removeIndirectLeftRecursion :: Grammar -> Grammar 
+--removeIndirectLeftRecursion = _
 
-removeDirectLeftRecursion :: ReductionRule 
+removeDirectLeftRecursion :: ReductionRule
+removeDirectLeftRecursion p@(Production src@(Nt s) xss) =
+  if null (filter ((== src) . head) xss)
+    then [p]
+    else consolidateProds . (epsProd :) . map f $ xss
+  where
+    src' = Nt (s ++ "'")
 
-applyLeftFactorization :: ReductionRule
-applyLeftFactorization (Production _ target) = foldl' f M.empty target 
-  where 
-    f :: [Symbol] -> Map Symbol [[Symbol]] -> Map 
-    f symbols map = 
+    epsProd = SingleProduction src' [Epsilon]
+
+    f :: [Symbol] -> SingleProduction
+    f (first:xs) =
+      if first /= src
+        then SingleProduction src  (xs ++ [src'])
+        else SingleProduction src' (xs ++ [src'])
+
+--applyLeftFactorization :: ReductionRule
+--applyLeftFactorization (Production _ target) = foldl' f M.empty target 
+--  where 
+--    f :: [Symbol] -> Map Symbol [[Symbol]] -> Map 
+--    f symbols map = _
--- app/TigerGrammar.hs
+++ app/TigerGrammar.hs
@@ -1,116 +1,191 @@
-module TigerGrammar where
+module TigerGrammar (tigerGrammar) where
 
 import Production
 
-"tiger_program"
-  := T "main" T "let" declaration_segment T "in" T "begin" stat_seq T "end"
-  ;
-"declaration_segment"
-  := var_declaration_list funct_declaration_list
-  ;
-"var_declaration_list"
-  := /*empty*/ 
-  | var_declaration var_declaration_list
-  ;
-"var_declaration"
-  := T "var" id_list T ":" type_ optional_init T ";"
-  ;
-"funct_declaration_list"
-  := /*empty*/ 
-  | funct_declaration funct_declaration_list
-  ;
-"funct_declaration"
-  := T "function" ID T "(" param_list T ")" ret_type T "begin" stat_seq T "end"
-  ;
-"type_"
-  := type_id 
-  | T "array" T "[" INTLIT T "]" T "of" type_id
-  ;
-"type_id"
-  := T "int" 
-  | T "float"
-  ;
-"id_list"
-  := ID 
-  | ID T "," id_list
-  ;
-"optional_init"
-  := /*empty*/ 
-  | T ":=" const
-  ;
-"param_list"
-  := /*empty*/ 
-  | param param_list_tail
-  ;
-"param_list_tail"
-  := /*empty*/ 
-  | T "," param param_list_tail
-  ;
-"ret_type"
-  := /*empty*/ 
-  | T ":" type_
-  ;
-"param"
-  := ID T ":" type_
-  ;
-"stat_seq"
-  := stat 
-  | stat stat_seq
-  ;
+tiger_program          = Nt "tiger_program"
+declaration_segment    = Nt "declaration_segment"
+var_declaration_list   = Nt "var_declaration_list"
+var_declaration        = Nt "var_declaration"
+funct_declaration_list = Nt "funct_declaration_list"
+funct_declaration      = Nt "funct_declaration"
+type_                  = Nt "type_"
+type_id                = Nt "type_id"
+id_list                = Nt "id_list"
+optional_init          = Nt "optional_init"
+param_list             = Nt "param_list"
+param_list_tail        = Nt "param_list_tail"
+ret_type               = Nt "ret_type"
+param                  = Nt "param"
+stat_seq               = Nt "stat_seq"
+stat                   = Nt "stat"
+opt_prefix             = Nt "opt_prefix"
+expr                   = Nt "expr"
+const_nt               = Nt "const"
+binary_operator        = Nt "binary_operator"
+expr_list              = Nt "expr_list"
+expr_list_tail         = Nt "expr_list_tail"
+lvalue                 = Nt "lvalue"
+lvalue_tail            = Nt "lvalue_tail"
 
-"stat"
-  := lvalue T ":=" expr T ";" 
-  | T "if" expr T "then" stat_seq T "endif" T ";" 
-  | T "if" expr T "then" stat_seq T "else" stat_seq T "endif" T ";" 
-  | T "while" expr T "do" stat_seq T "enddo" T ";" 
-  | T "for" ID T ":=" expr T "to" expr T "do" stat_seq T "enddo" T ";"
-  | opt_prefix ID T "(" expr_list T ")" T ";" 
-  | T "break" T ";" 
-  | T "return" expr T ";" 
-  | T "let" declaration_segment T "in" stat_seq T "end" 
-  ;
+main'                  = T "main"
+let'                   = T "let"
+in'                    = T "in"
+begin'                 = T "begin"
+end'                   = T "end"
+var'                   = T "var"
+int'                   = T "int"
+float'                 = T "float"
+id'                    = T "ID"
+of'                    = T "of"
+break'                 = T "break"
+return'                = T "return"
+intlit'                = T "INTLIT"
+floatlit'              = T "FLOATLIT"
+function'              = T "function"
+array'                 = T "array"
+enddo'                 = T "enddo"
+do'                    = T "do"
+to'                    = T "to"
+for'                   = T "for"
+while'                 = T "while"
+endif'                 = T "endif"
+else'                  = T "else"
+then'                  = T "then"
+if'                    = T "then"
 
-"opt_prefix"
-  := /*empty*/ 
-  | lvalue T ":="
-  ;
-"expr"
-  := const 
-  | lvalue 
-  | expr binary_operator expr 
-  | T "(" expr T ")"
-  ;
-"const"
-  := INTLIT 
-  | FLOATLIT
-  ;
-"binary_operator"
-  := T "+"
-  | T "-"
-  | T "*"
-  | T "/"
-  | T "="
-  | T "<>"
-  | T "<"
-  | T ">"
-  | T "<="
-  | T ">="
-  | T "&"
-  | T "|"
-  ;
-"expr_list"
-  := /*empty*/ 
-  | expr expr_list_tail
-  ;
-"expr_list_tail"
-  := /*empty*/ 
-  | T "," expr expr_list_tail
-  ;
-"lvalue"
-  := ID lvalue_tail
-  ;
-"lvalue_tail"
-  := /*empty*/ 
-  | T "[" expr T "]"
-  ;
+tigerGrammar :: Grammar
+tigerGrammar =
+  [ tiger_program
+      =: rule main' # let' # declaration_segment # declaration_segment
+        # in' # begin' # stat_seq # end'
+      ## end_production
 
+  , declaration_segment
+      =: rule var_declaration_list # funct_declaration_list
+      ## end_production
+
+  , var_declaration_list
+      =: rule Epsilon
+      |. rule var_declaration # var_declaration_list
+      ## end_production
+
+  , var_declaration
+      =: rule var' # id_list # T ":" # type_ # optional_init # T ";"
+      ## end_production
+ 
+  , funct_declaration_list
+      =: rule Epsilon
+      |. rule funct_declaration # funct_declaration_list
+      ## end_production
+
+  , funct_declaration
+      =: rule function' # id' # T "(" # param_list  # T ")" #
+          ret_type # begin' # stat_seq # end'
+      ## end_production
+
+  , type_
+      =: rule type_id
+      |. rule array' # T "[" # intlit' # T "]" # of' # type_id
+      ## end_production
+
+  , type_id 
+      =: rule int' 
+      |. rule float'
+      ## end_production
+
+  , id_list 
+      =: rule id'
+      |. rule id' # T "," # id_list
+      ## end_production
+
+  , optional_init 
+      =: rule (T ":=") # const_nt
+      ## end_production
+
+  , param_list 
+      =: rule Epsilon
+      |. rule param # param_list_tail
+      ## end_production
+
+  , param_list_tail 
+      =: rule Epsilon
+      |. rule (T ",") # param # param_list_tail
+      ## end_production
+
+  , ret_type 
+      =: rule Epsilon
+      |. rule (T ":") # type_
+      ## end_production
+
+  , param 
+      =: rule id' # (T ":") # type_
+      ## end_production
+
+  , stat_seq 
+      =: rule stat
+      |. rule stat # stat_seq
+      ## end_production
+
+  , stat
+      =: rule lvalue # T ":=" # expr # T ";"
+      |. rule if' # expr # then' # stat_seq # endif' # T ";"
+      |. rule if' # expr # then' # stat_seq # else' # stat_seq # endif' # T ";"
+      |. rule while' # expr # do' # stat_seq # enddo' # T ";"
+      |. rule for' # id' # T ":=" # expr # to' # expr # do' # stat_seq # enddo' # T ";"
+      |. rule opt_prefix # id' # T "(" # expr_list # T ")" # T ";"
+      |. rule break' # T ";"
+      |. rule return' # expr # T ";"
+      |. rule let' # declaration_segment # in' # stat_seq # end' 
+      ## end_production
+
+  , opt_prefix
+      =: rule Epsilon
+      |. rule lvalue # T ":="
+      ## end_production
+
+  , expr
+      =: rule const_nt
+      |. rule lvalue 
+      |. rule expr # binary_operator # expr 
+      |. rule (T "(") # expr # T ")"
+      ## end_production
+
+  , const_nt
+      =: rule intlit'
+      |. rule floatlit'
+      ## end_production
+
+  , binary_operator
+      =: rule (T "+")
+      |. rule (T "-")
+      |. rule (T "*")
+      |. rule (T "/")
+      |. rule (T "=")
+      |. rule (T "<>")
+      |. rule (T "<")
+      |. rule (T ">")
+      |. rule (T "<=")
+      |. rule (T ">=")
+      |. rule (T "&")
+      |. rule (T "|")
+      ## end_production
+
+  , expr_list
+      =: rule Epsilon
+      |. rule expr # expr_list_tail
+      ## end_production
+
+  , expr_list_tail
+      =: rule Epsilon
+      |. rule (T ",") # expr # expr_list_tail
+      ## end_production
+
+  , lvalue
+      =: rule id' # lvalue_tail
+      ## end_production
+
+  , lvalue_tail
+      =: rule Epsilon
+      |. rule (T "[") # expr # T "]"
+      ## end_production
+  ]
